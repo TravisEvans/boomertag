ef7710e0d72bcc01a731e171100711a7973d5901
Celery <51399086+TravisEvans@users.noreply.github.com>
2024-12-15 21:44:55 +1100

    It fucking works, the air strafiung fucking works
    somethingsomething clamping max and min numbers allowed the "surfing" or air strafe acceleration to work.
    Oh baby thank god

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody3D
 
 const SPEED = 10.0
 const SPRINT_SPEED = 20.0
-const AIR_STRAFE_SPEED = 5.0
+const AIR_STRAFE_ACCELERATION = 500.0 # this is HUGE because of 
+const SPEED_LIMIT = 0.8
 const CROUCH_SPEED_MODIFIER = 0.5
 const JUMP_VELOCITY = 5.5
 const SENSITIVITY = 0.004
@@ -17,7 +18,6 @@ const CROUCH_HEIGHT = 1.2
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-
 func _unhandled_input(event): # originally yoinked from https://github.com/LegionGames/FirstPersonController
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * SENSITIVITY)
@@ -26,12 +26,13 @@ func _unhandled_input(event): # originally yoinked from https://github.com/Legio
 
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
 	# Handle jump. 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	# Handle crouch
 	crouch(Input.is_action_pressed("crouch"))
@@ -41,29 +42,30 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction and Input.is_action_pressed("sprint") and is_on_floor(): # check if sprinting on ground
-		velocity.x = move_toward(velocity.x, (direction.x * SPRINT_SPEED), 1.0)
-		velocity.z = move_toward(velocity.z, (direction.z * SPRINT_SPEED), 1.0)
+		velocity.x = lerp(velocity.x, (direction.x * SPRINT_SPEED), 0.05)
+		velocity.z = lerp(velocity.z, (direction.z * SPRINT_SPEED), 0.05)
 	elif direction and is_on_floor():
-		velocity.x = move_toward(velocity.x, (direction.x * SPEED), 0.5)
-		velocity.z = move_toward(velocity.z, (direction.z * SPEED), 0.5)
+		velocity.x = lerp(velocity.x, (direction.x * SPEED), 0.025)
+		velocity.z = lerp(velocity.z, (direction.z * SPEED), 0.025)
 	elif is_on_floor():
 		velocity.x = move_toward(velocity.x, 0.0, 0.5)
 		velocity.z = move_toward(velocity.z, 0.0, 0.5)
 	
-	#Handle air strafing IM FUCKING TRYIN
-	#if (Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right")) and !is_on_floor():
-		#velocity.x += direction.x * AIR_STRAFE_SPEED
-		#velocity.z += direction.z * AIR_STRAFE_SPEED
-	#if (Input.is_action_just_released("left") or Input.is_action_just_released("right")) and !is_on_floor():
-		#velocity.x -= direction.x * AIR_STRAFE_SPEED
-		#velocity.z -= direction.z * AIR_STRAFE_SPEED
+	# Handle air strafing -- https://www.willdonnelly.net/blog/2021-05-16-godot-airstrafe-controller/
+	# Project current velocity onto the strafe direction, and compute a capped
+	# acceleration such that *projected* speed will remain within the limit.
+	var currentSpeed = direction.dot(velocity)
+	var accel = AIR_STRAFE_ACCELERATION * delta
+	accel = max(0, min(accel, SPEED_LIMIT - currentSpeed))
+	# Apply strafe acceleration to velocity and then integrate motion
+	velocity += direction * accel
 	
 	# Checks for crouching to modify crouch walk speed
 	if Input.is_action_pressed("crouch") and is_on_floor():
 		velocity.x *= CROUCH_SPEED_MODIFIER
 		velocity.z *= CROUCH_SPEED_MODIFIER
 	
-	updateVelocityLabel(velocity.x, velocity.z)
+	updateDEBUGLabel(velocity.x, velocity.z, direction)
 	# I have no fucking clue how effective this is
 	move_and_slide()
 
@@ -78,5 +80,6 @@ func crouch(crouchState: bool):
 			#$AnimationPlayer.play("idle") for future reference
 
 
-func updateVelocityLabel(velX: float, velZ: float) -> void: #DEBUG
-	uiTempLabel.text = "Current velocity: " + str(snapped(velX, 0.01)) + ", " + str(snapped(velZ, 0.01))
+func updateDEBUGLabel(velX: float, velZ: float, dir: Vector3) -> void: #DEBUG
+	uiTempLabel.text = "Current velocity: " + str(snapped(velX, 0.01)) + ", " + str(snapped(velZ, 0.01)) + " 
+	Current absolute direction: " + str(snapped(dir.x,0.01)) + "," + str(snapped(dir.z,0.01))
