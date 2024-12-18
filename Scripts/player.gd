94b85f29f6347ebd8da72539b36f46c71e9c8ccd
Celery <51399086+TravisEvans@users.noreply.github.com>
2024-12-18 21:49:39 +1100

    added STRAFTAT map assets
    idk what else i did

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,7 +7,7 @@ const GROUND_ACCELERATION = 1000.0
 const SPEED_LIMIT = 1.2
 const CROUCH_SPEED = 5.0
 const JUMP_VELOCITY = 5.5
-const SENSITIVITY = 0.004
+const SENSITIVITY = 0.003
 const HEIGHT = 2.0
 const CROUCH_HEIGHT = 1.2
 
@@ -15,6 +15,7 @@ const CROUCH_HEIGHT = 1.2
 @onready var uiTempLabel = get_tree().current_scene.get_node("UI/HUD/Label") ##DEBUG
 #@onready var cameraPivot = $CameraPivot # the "head" for rotation, idk check this for more info: https://docs.godotengine.org/en/4.0/tutorials/3d/using_transforms.html
 
+var wall_jump_count := 0
 
 
 func _ready():
@@ -29,17 +30,26 @@ func _unhandled_input(event): # originally yoinked from https://github.com/Legio
 
 func _physics_process(delta: float) -> void:
 	
+	if is_on_floor(): # reset wall jump count
+		wall_jump_count = 0
+	if is_on_wall_only():
+		velocity = velocity.slide(get_wall_normal())
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	# Handle jump. 
-	if Input.is_action_pressed("jump") and is_on_floor():
+
+	if Input.is_action_pressed("jump") and is_on_floor(): # Handle jump
 		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("jump") and is_on_wall_only() and wall_jump_count == 0: # Handle wall jump
+		velocity.y = JUMP_VELOCITY
+		velocity.x += (get_wall_normal().x*velocity.x)*0.5 if velocity.x>0 else -(get_wall_normal().x*velocity.x)*0.5
+		velocity.z += (get_wall_normal().z*velocity.z)*0.5 if velocity.z>0 else -(get_wall_normal().z*velocity.z)*0.5
+		wall_jump_count+=1
 	elif direction and Input.is_action_pressed("sprint") and is_on_floor() and !Input.is_action_pressed("crouch"): # check if sprinting on ground and NOT crouching
-		velocity.x = lerp(velocity.x, (direction.x * SPRINT_SPEED), 0.05)
-		velocity.z = lerp(velocity.z, (direction.z * SPRINT_SPEED), 0.05)
+		velocity.x = lerp(velocity.x, (direction.x * SPRINT_SPEED), 0.1)
+		velocity.z = lerp(velocity.z, (direction.z * SPRINT_SPEED), 0.1)
 	elif velocity.length() > CROUCH_SPEED and is_on_floor() and Input.is_action_pressed("crouch"): # check if crouching on ground with momentum, aka slide
 		# this checks if ground is flat or otherwise
 		var changingVelocity = 0.25 if (get_floor_normal() == Vector3.UP) or get_floor_angle()>deg_to_rad(60.0) else 0.9
@@ -48,8 +58,6 @@ func _physics_process(delta: float) -> void:
 		# apply to velocity as normal, with modifiers
 		velocity.x = lerp(velocity.x, velocity.slide(get_floor_normal()).x*changingVelocity, 0.01)
 		velocity.z = lerp(velocity.z, velocity.slide(get_floor_normal()).z*changingVelocity, 0.01)
-		print(rad_to_deg(get_floor_angle()))
-		print(changingVelocity)
 	elif direction and Input.is_action_pressed("crouch") and is_on_floor(): # check if crouching on ground
 		velocity.x = lerp(velocity.x, (direction.x * CROUCH_SPEED), 0.1)
 		velocity.z = lerp(velocity.z, (direction.z * CROUCH_SPEED), 0.1)
@@ -60,32 +68,27 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0.0, 0.5)
 		velocity.z = move_toward(velocity.z, 0.0, 0.5)
 	
-	if Input.is_action_just_pressed("debugQuery"): ##DEBUG
-		#print("debugging")
-		print("
-		normal: " + str(get_floor_normal()) + "
-		NORMAL lngth: " + str(get_floor_normal().length()) + "
-		vel.slide(nrm): " + str(velocity.slide(get_floor_normal())))
-	if Input.is_action_just_pressed("debugAction"): ##DEBUG
-		velocity*=3
-	
-	# Add the gravity.
+	# Add gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += (get_gravity() * delta) if not is_on_wall() else (get_gravity() * delta)/1.375
 	# Handle crouching position change
 	crouch(Input.is_action_pressed("crouch"))
 	
 	# Handle air strafing -- https://www.willdonnelly.net/blog/2021-05-16-godot-airstrafe-controller/
 	# Project current velocity onto the strafe direction, and compute a capped
 	# acceleration such that *projected* speed will remain within the limit.
-	var currentSpeed = direction.dot(velocity)
-	var accel = GROUND_ACCELERATION * delta if is_on_floor() else AIR_STRAFE_ACCELERATION * delta
-	accel = max(0, min(accel, SPEED_LIMIT - currentSpeed))
-	# Apply strafe acceleration to velocity and then integrate motion
-	velocity += direction * accel
+	#var currentSpeed = direction.dot(velocity)
+	#var accel = GROUND_ACCELERATION * delta if is_on_floor() else AIR_STRAFE_ACCELERATION * delta
+	#accel = max(0, min(accel, SPEED_LIMIT - currentSpeed))
+	## Apply strafe acceleration to velocity and then integrate motion
+	#velocity += direction * accel
 	
 	
-	updateDEBUGLabel(direction)
+	if Input.is_action_just_pressed("debugQuery"): ##DEBUG
+		print("debugging")
+	if Input.is_action_just_pressed("debugAction"): ##DEBUG
+		velocity*=3
+	updateDEBUGLabel(direction) ##DEBUG
 	# I have no fucking clue how effective this is
 	move_and_slide()
 # END of _physics_process
